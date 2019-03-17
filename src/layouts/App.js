856f1df0a8cf981163a010776cca6ebee1ea3ee9
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-17 23:15:46 +0100

    Add project structure

---
+++ src/layouts/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default App;
