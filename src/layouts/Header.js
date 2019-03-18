8c448b839a8aff8acd551d1374c2c0dc6f2a866d
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-18 23:32:36 +0100

    Create Header component

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "../styles/Header.css";
+import foto1 from "../images/header1.jpg";
+import foto2 from "../images/header2.jpg";
+import foto3 from "../images/header3.jpg";
 
 const Header = props => {
-  return <div>Header</div>;
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact render={() => <img src={foto1} alt="miasto" />} />
+        <Route
+          path="/products"
+          render={() => <img src={foto2} alt="miasto" />}
+        />
+        <Route
+          path="/contact"
+          render={() => <img src={foto3} alt="miasto" />}
+        />
+        <Route path="/admin" render={() => <img src={foto3} alt="miasto" />} />
+        <Route render={() => <img src={foto1} alt="miasto" />} />
+      </Switch>
+    </>
+  );
 };
 
 export default Header;
