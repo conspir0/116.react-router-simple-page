15358b593fa25d0ef3e8db4cfc276d40cdb4a724
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-18 23:00:05 +0100

    Create Navigation component

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const list = [
+  { name: "home", path: "/", exact: true },
+  { name: "products", path: "/products" },
+  { name: "contact", path: "/contact" },
+  { name: "admin", path: "/admin" }
+];
 
 const Navigation = () => {
-  return <div>Navigations</div>;
+  const menu = list.map(item => {
+    return (
+      <li key="item.name">
+        <NavLink to={item.path} exact={item.exact ? item.exact : false}>
+          {item.name}
+        </NavLink>
+      </li>
+    );
+  });
+
+  return (
+    <nav>
+      <ul>{menu}</ul>
+    </nav>
+  );
 };
 
 export default Navigation;
