3c29f9a257538d851350562e206e97e6ba3e6e63
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-20 22:31:48 +0100

    Create Article component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,35 @@
 import React from "react";
+import Article from "../components/Article";
+
+const articles = [
+  {
+    id: 1,
+    title: "Czym jest teoria strun",
+    author: "Jan Nowak",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ab mollitia sunt ducimus possimus, expedita commodi nesciunt quam dolores, delectus voluptatem minus quisquam, sapiente labore in animi cum dolor quo!"
+  },
+  {
+    id: 2,
+    title: "Czym jest paradoks Fermiego",
+    author: "Adam Kowalski",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ab mollitia sunt ducimus possimus, expedita commodi nesciunt quam dolores, delectus voluptatem minus quisquam, sapiente labore in animi cum dolor quo!"
+  },
+  {
+    id: 3,
+    title: "Ciemna energia i ciemna materia",
+    author: "Bogdan Energia",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ab mollitia sunt ducimus possimus, expedita commodi nesciunt quam dolores, delectus voluptatem minus quisquam, sapiente labore in animi cum dolor quo!"
+  }
+];
 
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const artList = articles.map(article => (
+    <Article id={article.id} {...article} />
+  ));
+  return <div>{artList}</div>;
 };
 
 export default HomePage;
